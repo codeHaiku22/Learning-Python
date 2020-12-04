e53efb08feb5117a71cdc36c7e2d16d1f383695b
codehaiku22 <mailstufftodeep@zoho.com>
2020-12-03 16:13:08 -0800

    Appended time.py.

--- time.py
+++ time.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+
+#time
 import time
 
 start = time.time()
@@ -6,6 +8,21 @@ start = time.time()
 time.sleep(2)
 
 stop = time.time()
+
+elapsed = stop - start
+
+print(elapsed)
+print("Total elapsed time:", float(elapsed), "seconds.")
+print("Total elapsed time:", int(elapsed), "seconds.")
+
+#timeit
+import timeit
+
+start = timeit.default_timer()
+
+time.sleep(2)
+
+stop = timeit.default_timer()
 
 elapsed = stop - start
 
