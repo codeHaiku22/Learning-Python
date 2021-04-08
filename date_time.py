b876bd08564c95b1f0b7cf1230db5e555a9551e0
codeHaiku22 <mailstufftodeep@zoho.com>
2021-04-08 16:44:50 -0700

    Updated date_time.py.

--- date_time.py
+++ date_time.py
@@ -41,28 +41,38 @@ print(x.strftime("%B %d, %Y"))                  #June 01, 2018
 """
 Directive   Description                                                     Example
 ---------   -----------------------------------------------------------     ------------------------   
-%a 	        Weekday, short version 	                                        Wed 	
-%A 	        Weekday, full version 	                                        Wednesday 	
-%w 	        Weekday as a number 0-6, 0 is Sunday 	                        3 	
-%d 	        Day of month 01-31 	                                            31 	
-%b 	        Month name, short version 	                                    Dec 	
-%B 	        Month name, full version 	                                    December 	
-%m 	        Month as a number 01-12 	                                    12 	
-%y 	        Year, short version, without century 	                        18 	
-%Y 	        Year, full version 	                                            2018 	
-%H 	        Hour 00-23 	                                                    17 	
-%I 	        Hour 00-12 	                                                    05 	
-%p 	        AM/PM 	                                                        PM 	
-%M 	        Minute 00-59 	                                                41 	
-%S 	        Second 00-59 	                                                08 	
-%f 	        Microsecond 000000-999999 	                                    548513 	
-%z 	        UTC offset 	                                                    +0100 	
-%Z 	        Timezone 	                                                     CST 	
-%j 	        Day number of year 001-366 	                                    365 	
-%U 	        Week number of year, Sunday as the first day of week, 00-53 	52 	
-%W 	        Week number of year, Monday as the first day of week, 00-53 	52 	
-%c 	        Local version of date and time 	                                Mon Dec 31 17:41:00 2018 	
-%x 	        Local version of date 	                                        12/31/18 	
-%X 	        Local version of time 	                                        17:41:00 	
-%% 	        A % character 	                                                %
+%a 	    Weekday, short version 	                                    Wed 	
+%A 	    Weekday, full version 	                                    Wednesday 	
+%w 	    Weekday as a number 0-6, 0 is Sunday 	                    3 	
+%d 	    Day of month 01-31 	                                            31 	
+%b 	    Month name, short version 	                                    Dec 	
+%B 	    Month name, full version 	                                    December 	
+%m 	    Month as a number 01-12 	                                    12 	
+%y 	    Year, short version, without century 	                    18 	
+%Y 	    Year, full version 	                                            2018 	
+%H 	    Hour 00-23 	                                                    17 	
+%I 	    Hour 00-12 	                                                    05 	
+%p 	    AM/PM 	                                                    PM 	
+%M 	    Minute 00-59 	                                            41 	
+%S 	    Second 00-59 	                                            08 	
+%f 	    Microsecond 000000-999999 	                                    548513 	
+%z 	    UTC offset 	                                                    +0100 	
+%Z 	    Timezone 	                                                    CST 	
+%j 	    Day number of year 001-366 	                                    365 	
+%U 	    Week number of year, Sunday as the first day of week, 00-53     52 	
+%W 	    Week number of year, Monday as the first day of week, 00-53     52 	
+%c 	    Local version of date and time 	                            Mon Dec 31 17:41:00 2018 	
+%x 	    Local version of date 	                                    12/31/18 	
+%X 	    Local version of time 	                                    17:41:00 	
+%% 	    A % character 	                                            %
 """
+
+#The strptime() Method
+#The datetime object has a method for formatting strings which represent dates/times into datetime objects.
+#It takes two parameters: string representing a date/time and format code.
+import datetime
+
+datetime_string = '4/8/2021 3:21:48 PM'
+datetime_object = datetime.datetime.strptime(datetime_string, '%m/%d/%Y %I:%M:%S %p')
+
+print(datetime_object.strftime('%m/%d/%Y %H:%M'))			#04/08/2021 15:21
